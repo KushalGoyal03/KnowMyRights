4042439ea9fc20668f927147af037d489b16ecd4
KushalGoyal03 <kushalgoyal011@gmail.com>
2025-07-16 19:11:47 +0530

    Update

--- backend/models/rightsModel.js
+++ backend/models/rightsModel.js
@@ -1,12 +1,15 @@
 // backend/models/rightsModel.js
 import mongoose from "mongoose";
 
-const rightSchema = new mongoose.Schema({
-  category: { type: String, required: true, unique: true },
-  summary: { type: String, required: true },
-  sources: [{ type: String }],
-  createdAt: { type: Date, default: Date.now },
-  lastUpdated: { type: Date, default: Date.now },
-});
+const rightSchema = new mongoose.Schema(
+  {
+    category: { type: String, required: true, unique: true },
+    summary: { type: String, required: true },
+    sources: [{ type: String }],
+  },
+  {
+    timestamps: true,
+  }
+);
 
 export default mongoose.model("Right", rightSchema);
